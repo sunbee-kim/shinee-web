09f7d4432e4fc675a83bad80b05bbe2b0f80cb72
ny.kim <ny.kim@dco.com>
2026-04-03 17:12:33 +0900

    feat: final

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
+"use client";
 import RocketR from "./rocket";
+import InternalMenu from "./tail-checker";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
                 sunbee@shinee.kr
               </a>
             </span>
+            <InternalMenu />
           </p>
         </div>
       </main>
