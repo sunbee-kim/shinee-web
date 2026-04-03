1878c24277d371913f4074d4cb42ef4b108161eb
ny.kim <ny.kim@dco.com>
2026-04-03 15:51:49 +0900

    fix: no asset prefix for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.GITHUB_ACTIONS === "true";
-const repo = "shinee-web"; // Change to your repo name if different
-
 const nextConfig: NextConfig = {
   output: "export",
-  assetPrefix: isGithubPages ? `/${repo}/` : undefined,
   images: {
     unoptimized: true,
   },
