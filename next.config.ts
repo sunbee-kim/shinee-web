96aa8e5041760099ec6064ecfe9b16e3dc8a0bc3
ny.kim <ny.kim@dco.com>
2026-04-03 14:22:54 +0900

    feat: deploy scripts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+const repo = "shinee-web"; // Change to your repo name if different
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  assetPrefix: isGithubPages ? `/${repo}/` : undefined,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
